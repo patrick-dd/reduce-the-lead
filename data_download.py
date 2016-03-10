31d9104a23a2043e9711a1a7124d2af147b5b064
patrick-dd <patrick.doupe@gmail.com>
2016-03-10 12:50:17 -0500

    removing county level data collection

--- data_download.py
+++ data_download.py
@@ -23,35 +23,11 @@ census_api_key = config.get('census_api', 'key')
 # setting up a census object
 c = Census(census_api_key, year=2013)
 
-
-print '2010 Census data'
+print '2013 American Community Survey data'
 print 'Constructing variables'
 
 ## collecting variables to match the 
 ## at risk groups as defined by the cdc
-
-variables = {}
-variables['total_housing'] = 'H00010001'
-variables['occupied_houses'] = 'H0030002'
-
-"""
-young children in 'racial-ethnic minority groups'
-
-I don't know what the CDC mean by this statement. 
-I could not find evidence of specific groups.
-
-After discussions with colleagues I selected children 
-under 5 identified as black, indigenous or latin.
-"""
-
-variables['black_m_u5'] = 'P012B003'
-variables['black_f_u5'] = 'P012B027'
-variables['indig_m_u5'] = 'P012C003'
-variables['indig_f_u5'] = 'P012C027'
-variables['latin_m_u5'] = 'P012H003'
-variables['latin_f_u5'] = 'P012H027'
-
-# young children of minority groups
 vars_acs_block = {}
 vars_acs_block['m_u5'] = 'B01001_003E'
 vars_acs_block['f_u5'] = 'B01001_027E'
@@ -63,6 +39,16 @@ vars_acs_block['built_1960s'] = 'B25034_007E'
 vars_acs_block['built_1950s'] = 'B25034_008E'
 vars_acs_block['built_1940s'] = 'B25034_009E'
 vars_acs_block['built_before_1940'] = 'B25034_010E'
+
+"""
+young children in 'racial-ethnic minority groups'
+
+I don't know what the CDC mean by this statement. 
+I could not find evidence of specific groups.
+
+After discussions with colleagues I selected children 
+under 5 identified as black, indigenous or latin.
+"""
 
 # backgrounds of children
 vars_acs_tract = {}
@@ -88,33 +74,6 @@ vars_acs_tract['poverty_level_u149_o100'] = 'B06012_003E'
 vars_acs_tract['poverty_level_u100_foreign'] = 'B06012_018E'
 vars_acs_tract['poverty_level_u149_o100_foreign'] = 'B06012_019E'
 
-# aggregates only available at the subdivision level
-vars_acs_county = {}
-# jobs that may have lead exposure
-vars_acs_county['workers_total'] = 'B24121_001E'
-#vars_acs_county['artists'] = 'B24121_143E'
-#vars_acs_county['auto_repair'] = 'B24121_388E'
-#vars_acs_county['mechanics'] = 'B24121_390E'
-#vars_acs_county['bus_truck_mechanics'] = 'B24121_391E'
-#vars_acs_county['heavy_vehicle_mechs'] = 'B24121_392E'
-#vars_acs_county['small_vehicle_mechs'] = 'B24121_393E'
-#vars_acs_county['natres_construction'] = 'B08124_005E'
-#vars_acs_county['recycle_material_collectors'] = 'B24126_523E'
-#vars_acs_county['construction_labourers'] = 'B24121_345E'
-#vars_acs_county['painters_construction_maintenance'] = 'B24121_352E'
-#vars_acs_county['pipelayers'] = 'B24121_354E'
-#vars_acs_county['steelworkers'] = 'B24121_359E'
-#vars_acs_county['construction_helpers'] = 'B24121_360E'
-#vars_acs_county['septic_tank_cleaners'] = 'B24121_367E'
-#vars_acs_county['aircraft_mechanics'] = 'B24121_387E'
-#vars_acs_county['welders'] = 'B24121_440E'
-#vars_acs_county['metal_plastic_heat'] = 'B24121_441E'
-#vars_acs_county['metal_plastic_layout'] = 'B24121_442E'
-#vars_acs_county['metal_plastic_plating'] = 'B24121_443E'
-#vars_acs_county['metal_plastic_other'] = 'B24121_445E'
-#vars_acs_county['furnace_operators'] = 'B24121_474E'
-#vars_acs_county['painting_workers'] = 'B24121_479E'
-
 print 'Downloading and storing database'
 
 # getting state fips
@@ -132,12 +91,6 @@ df_tmp = pd.DataFrame(
 
 tmp_county_fips = df_tmp['county']
 
-df_county = pd.DataFrame(
-        c.acs5.state_county(
-            vars_acs_county.values(),
-            state_fips[0],
-            tmp_county_fips[0]))
-
 df_blocks = pd.DataFrame(
         c.acs5.state_county_blockgroup(
             vars_acs_block.values(),
@@ -154,13 +107,6 @@ df_tracts = pd.DataFrame(
 
 # looping through other counties in the first state
 for county_fip in tmp_county_fips[1:]:
-    # counties
-    df_tmp = pd.DataFrame(
-            c.acs5.state_county(
-                vars_acs_county.values(),
-                state_fips[0],
-                county_fip))
-    df_county = pd.concat([df_county, df_tmp])
     # blocks
     df_tmp = pd.DataFrame(
             c.acs5.state_county_blockgroup(
@@ -192,13 +138,6 @@ for state_fip in state_fips[1:]:
         tmp_county_fips = df_tmp['county']
         # looping through counties
         for county_fip in tmp_county_fips:
-            # counties
-            df_tmp = pd.DataFrame(
-                c.acs5.state_county(
-                    vars_acs_county.values(),
-                    state_fip,
-                    county_fip))
-            df_county = pd.concat([df_county, df_tmp])
             # blocks
             df_tmp = pd.DataFrame(
                 c.acs5.state_county_blockgroup(
@@ -216,15 +155,12 @@ for state_fip in state_fips[1:]:
                     Census.ALL))
             df_tracts = pd.concat([df_tracts, df_tmp])
     except KeyError:
-        print 'KeyError'
         pass
 
 # swapping keys and values of dictionary for dataframe relabeling
-vars_acs_county = {y:x for x, y in vars_acs_county.iteritems()}
 vars_acs_tract = {y:x for x, y in vars_acs_tract.iteritems()}
 vars_acs_block = {y:x for x, y in vars_acs_block.iteritems()}
 
-df_county.rename(columns = vars_acs_county, inplace=True)
 df_tracts.rename(columns = vars_acs_tract, inplace=True)
 df_blocks.rename(columns = vars_acs_block, inplace=True)
 
@@ -232,32 +168,33 @@ print 'Merging databases'
 df = pd.merge(df_blocks, df_tracts,
         on = ['state', 'county', 'tract'],
         how = 'outer')
-#df = pd.merge(df, df_county,
-#        on = ['state', 'county'],
-#        how = 'outer')
 
 df.rename(columns = {'block group': 'blockgroup'}, inplace = True)
 
 print 'Creating new variables'
 
-#df['old_houses'] = df.built_1970s + df.built_1960s + df.built_1950s + \
-#                    df.built_1940s + df.built_before_1940
+old_houses = df.built_1970s + df.built_1960s + df.built_1950s + df.built_1940s+\
+            df.built_before_1940
 
-#df['minority_children'] = df.m_u5_black + df.m_u5_indig + df.m_u5_latin + \
-#                    df.f_u5_black + df.f_u5_indig + df.f_u5_latin
+df['old_houses'] = old_houses
 
-#df['foreign_u6'] = df.foreign_parents_u6_foreign + \
-#                    df.one_foreign_parent_u6_foreign + \
-#                    df.one_foreign_parent_one_us_parent_u6_foreign
+minority_children = df.f_u5_indig + df.m_u5_indig + \
+                    df.f_u5_black + df.m_u5_black + \
+                    df.f_u5_latin + df.m_u5_latin
 
-#df['poverty_level'] = df.poverty_level_u100 + df.poverty_level_u149_o100 
+df['minority_children'] = minority_children
 
-#df['poverty_foreign'] = df.poverty_level_u100_foreign + \
-#                    df.poverty_level_u149_o100_foreign
+u6_foreign = df.foreign_parents_u6_foreign + \
+        df.one_foreign_parent_u6_foreign + \
+        df.one_foreign_parent_one_us_parent_u6_foreign \
 
-#df = df[['state', 'county', 'tract', 'blockgroup',\
-#        'old_houses', 'minority_children', 'foreign_u6',\
-#        'poverty_level', 'poverty_foreign']]
+df['foreign'] = u6_foreign
+
+povlvl = df.poverty_level_u100 + df.poverty_level_u149_o100
+povlvl_f = df.poverty_level_u100_foreign + df.poverty_level_u149_o100_foreign
+
+df['poverty_level'] = povlvl
+df['poverty_level_foreign'] = povlvl_f
 
 print 'Saving dataframe to csv'
 df.to_csv('lead_ranking_data.csv')
